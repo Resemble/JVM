a912ed2c2ab3469e24de2ec630b4e0e840f54e7b
ranran <931094193@qq.com>
2017-07-24 14:56:19 +0800

    VectorThreadSaveTest

---
+++ src/Concurrent/VectorThreadSaveTest.java
@@ -0,0 +1,50 @@
+package Concurrent;
+import java.util.Vector;
+
+/**
+ * @author ranran
+ * @version V1.0
+ * @Title:
+ * @Package PACKAGE_NAME
+ * @Description:
+ * @date 2017/7/24 10:15
+ */
+public class VectorThreadSaveTest {
+
+    private static Vector<Integer> vector = new Vector<Integer>();
+
+    public static void main(String[] args) {
+        while(true) {
+            for (int i = 0; i < 10; i++) {
+                vector.add(i);
+            }
+
+            Thread removeThread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for (int i = 0; i < vector.size(); i++) {
+                        vector.remove(i);
+                    }
+                }
+            });
+
+
+            Thread printThread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for (int i = 0; i < vector.size(); i++) {
+                        System.out.println(vector.get(i));
+                    }
+                }
+            });
+
+            removeThread.start();
+            printThread.start();
+
+            // 不要同时产生过多的线程，否则会导致操作系统假死
+            while (Thread.activeCount() > 20);
+
+        }
+    }
+
+}
