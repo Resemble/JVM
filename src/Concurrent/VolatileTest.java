28ec3a4154605f52a254fb4ecdfd23d3c2d66da6
ranran <931094193@qq.com>
2017-07-24 14:57:18 +0800

    modify

--- src/Concurrent/VolatileTest.java
+++ src/Concurrent/VolatileTest.java
@@ -5,8 +5,12 @@ package Concurrent;
  * @version V1.0
  * @Title:
  * @Package Concurrent
- * @Description:
+ * @Description: Volatile 变量对所有线程的可见性，一条线程修改了变量的值，
+ * 新值对其他线程来说总是可以立即得知的
  * @date 2017/7/24 11:00
  */
 public class VolatileTest {
+
+
+
 }
