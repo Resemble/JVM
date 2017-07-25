dc8b3f065ab0cadcb528c3648717835138f18f72
ranran <931094193@qq.com>
2017-07-25 21:16:17 +0800

    回文串

--- src/Java/Main2.java
+++ src/Java/Main2.java
@@ -1,46 +1,54 @@
 package Java;
+import java.util.HashMap;
 import java.util.Scanner;
+
 /**
  * @author ranran
  * @version V1.0
  * @Title:
  * @Package Java
- * @Description:
+ * @Description: 判断回文
  * @date 2017/7/25 19:52
  */
-public class Main {
+public class Main2 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String source = sc.nextLine();
         int length = source.length();
-        if (length / 2 == 1) {
-            return;
-        }
-        length = length - 2;
-        source = source.substring(0, length);
-        while (!judgeString(source, length)) {
-
-            length = length - 2;
-            if (length == 0) {
-                return;
-            }
-            source = source.substring(0, length);
-        }
-        System.out.println(length);
-
-    }
-
-    public static boolean judgeString(String source, int length) {
-        int halfLength = length / 2;
-        String begin = source.substring(0, halfLength);
-        String end = source.substring(halfLength, length);
-
-        if (begin.equals(end)) {
-            return true;
+        if (length == 1) {
+            System.out.println(1);
         } else {
-            return false;
+            char[] sourceChar = source.toCharArray();
+            HashMap<Character, Integer> hashMap = new HashMap<>();
+            for (int i = 0; i < source.length(); i++) {
+                char key = sourceChar[i];
+
+                if (hashMap.containsKey(key)) {
+                    int value = hashMap.get(key);
+                    if (value == 0) {
+                        value++;
+                        hashMap.put(key, value);
+                    } else {
+                        value--;
+                        hashMap.put(key, value);
+                    }
+
+                } else {
+                    hashMap.put(key, 1);
+                }
+            }
+            int count = 0;
+            for(Integer value : hashMap.values()) {
+                if (value == 1) {
+                    count++;
+                }
+            }
+            System.out.println(count);
         }
+
+
     }
+
 
 }
