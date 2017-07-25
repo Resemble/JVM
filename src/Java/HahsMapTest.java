ff66e5f2a9997a92d437864105f7036e33de2802
ranran <931094193@qq.com>
2017-07-25 18:04:44 +0800

    HashMap 遍历测试

---
+++ src/Java/HahsMapTest.java
@@ -0,0 +1,35 @@
+package Java;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author ranran
+ * @version V1.0
+ * @Title:
+ * @Package Java
+ * @Description:
+ * @date 2017/7/25 17:20
+ */
+public class HahsMapTest {
+
+    public static void main(String[] args) {
+
+        HashMap<Integer, String> hashMap = new HashMap();
+        hashMap.put(2, "23");
+        hashMap.put(22, "232");
+        hashMap.put(21, "er3");
+
+        for (Map.Entry<Integer, String> entry : hashMap.entrySet()) {
+            System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
+        }
+
+        for (Integer key : hashMap.keySet()) {
+            System.out.println("Key = " + key);
+        }
+        for (String value : hashMap.values()) {
+            System.out.println("Value = " + value);
+        }
+    }
+
+}
