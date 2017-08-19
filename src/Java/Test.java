c1844b9f9cdfcef9d1c4f30f0f7480956e4fdb8e
ranran <931094193@qq.com>
2017-08-19 22:21:31 +0800

    fail fast test

--- src/Java/Test.java
+++ src/Java/Test.java
@@ -1,5 +1,8 @@
 package Java;
 
+import java.util.Iterator;
+import java.util.Vector;
+
 /**
  * @author ranran
  * @version V1.0
@@ -16,6 +19,33 @@ public class Test {
         System.out.println("\"");
         System.out.println("'");
         System.out.println("/'");
+
+        failFastTest();
+
+    }
+
+
+    public static void failFastTest() {
+        Vector<Integer> vector = new Vector<>();
+        vector.add(0);
+        vector.add(1);
+        vector.add(2);
+        vector.add(3);
+        vector.add(4);
+        Iterator<Integer> integerIterator = vector.iterator();
+        int j = 0;
+        while (integerIterator.hasNext()) {
+            System.out.println(integerIterator.next());
+            if (j == 3) {
+                vector.remove(0);
+                integerIterator.remove();
+            }
+            j++;
+        }
+//        Iterator<Integer> integerIterator2 = vector.iterator();
+//        while (integerIterator2.hasNext()) {
+//            System.out.println(integerIterator2.next());
+//        }
     }
 
 }
