eaf876e05e13cd8c6381ec0ec7ae2bf12e80949b
ranran <931094193@qq.com>
2017-08-22 17:53:44 +0800

    round 四舍五入

--- src/Java/MathTest.java
+++ src/Java/MathTest.java
@@ -6,7 +6,19 @@ package Java;
  * @Title:
  * @Package Java
  * @Description:
+ * ceil()：将小数部分一律向整数部分进位。
+    floor()：一律舍去，仅保留整数。
+    round()：进行四舍五入
  * @date 2017/8/22 10:21
  */
 public class MathTest {
+
+    public static void main(String[] args) {
+        System.out.println(Math.round(4.5));
+        System.out.println(Math.round(-4.5));
+        System.out.println(Math.floor(4.5));
+        System.out.println(Math.ceil(4.5));
+        System.out.println(1/3*3);
+    }
+
 }
