e2d16c2b8c980e642517fd023241563dfe259e7d
ranran <931094193@qq.com>
2017-08-24 22:11:34 +0800

    静态方法测试

--- src/Java/Derived.java
+++ src/Java/Derived.java
@@ -12,11 +12,18 @@ class Base {
     public Base(String s) {
         System.out.println("B");
     }
+
+    public static void testStatic() {
+        System.out.println("Base static!");
+    }
 }
 public class Derived extends Base{
     public Derived(String s) {
         super(s);
         System.out.println("D");
+    }
+    public static void testStatic() {
+        System.out.println("Derived static!");
     }
 
     public static void main(String[] args) {
