d1c76f25f7fa0913ae4013243ad1f8d9d06ed6b6
ranran <931094193@qq.com>
2017-08-27 17:34:03 +0800

    线程终止

--- src/Thread/StopExample2.java
+++ src/Thread/StopExample2.java
@@ -8,25 +8,34 @@ package Thread;
  * @Description:
  * @date 2017/8/26 10:33
  */
-public class StopExample1 extends Thread{
+public class StopExample2 extends Thread {
 
 
-    public static void main(String[] args) {
-        StopExample1 thread = new StopExample1();
-        System.out.println("Start thread ...");
-        thread.start();
-//        thread.run();
-        thread.stop();
-        System.out.println("Stop application");
+    public static void main(String[] args) throws Exception {
+        StopExample2 stopExample2 = new StopExample2();
+        stopExample2.start();
+        stopExample2.interrupt();
     }
 
+    @Override
     public void run() {
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        System.out.println("start run ------------");
+        int i = 0;
+        while (true) {
+
+            if (Thread.interrupted()) {
+                System.out.println("true ******** ");
+            } else {
+//                System.out.println("false ****** ");
+            }
+//            try {
+//                Thread.sleep(2000);
+//                System.out.println("wake up");
+//            } catch (InterruptedException e) {
+//                System.out.println("catch interrupted exception");
+//                return;
+//            }
         }
-        System.out.println("this thread is running...");
     }
 
 }
--- src/Thread/StopExample3.java
+++ src/Thread/StopExample3.java
@@ -8,5 +8,34 @@ package Thread;
  * @Description:
  * @date 2017/8/27 16:25
  */
-public class StopExample3 {
+public class StopExample3 extends Thread{
+
+    public static void main(String[] args) throws Exception {
+        StopExample3 stopExample3 = new StopExample3();
+        stopExample3.start();
+        stopExample3.interrupt();
+    }
+
+    @Override
+    public void run() {
+        System.out.println("start run ------------");
+        int i = 0;
+        while (true) {
+
+            if (Thread.interrupted()) {
+                System.out.println("true ******** ");
+            } else {
+//                System.out.println("false ****** ");
+            }
+//            try {
+//                Thread.sleep(2000);
+//                System.out.println("wake up");
+//            } catch (InterruptedException e) {
+//                System.out.println("catch interrupted exception");
+//                return;
+//            }
+        }
+    }
+
+
 }
